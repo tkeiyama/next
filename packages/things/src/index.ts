78e4b5816903f9f101f4a405e5f4fd1227e56fad
tkeiyama <thomas18thx@gmail.com>
2022-05-17 20:30:15 +0900

    Add an implementation for GraphQL with mercurius

--- packages/things/src/index.ts
+++ packages/things/src/index.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
+import mercurius from 'mercurius'
 
 const app = fastify({logger:true})
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async(_: unknown, {x,y}: {x:number,y:number}) => {
+      return x+y
+    }
+  }
+}
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true
+})
 
 app.get('/', (_, rep) => {
   rep.send({hello: 'world'})
