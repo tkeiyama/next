f391659b949eb2987c4dcd8764908e0bba0dc5f0
tkeiyama <thomas18thx@gmail.com>
2022-05-17 20:22:37 +0900

    Implement a server with fastify

--- packages/things/src/index.ts
+++ packages/things/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!")
+import fastify from 'fastify'
+
+const app = fastify({logger:true})
+
+app.get('/', (_, rep) => {
+  rep.send({hello: 'world'})
+})
+
+app.listen(3000, (err, addr) => {
+  if(err) {
+    app.log.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Listening at ${addr}`)
+  }
+})
